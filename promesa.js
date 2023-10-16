7ea696c631c0110b2b0d297476327b277990b705
jegiraldp <jegiraldp@gmail.com>
2023-10-16 14:09:18 -0500

    161023

--- promesa.js
+++
@@ -1,6 +0,0 @@
-module.exports.contador=function(){
-    return new Promise(function (resolve,reject){
-      var saludo="Hola promesa sin retorno";
-      return resolve(saludo);
-  });
-}//
--- test_promesa.js
+++
@@ -1,6 +0,0 @@
-const lis = require('./promesa.js');
-console.log("vamos bien");
-var promesa = lis.contador();
-promesa.then(function(resultado) {
-  console.log(resultado);
-});

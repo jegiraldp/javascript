28bcbb07adefdfbc8c022dec96c9b26e2dae48ca
jegiraldp <jegiraldp@gmail.com>
2024-05-18 22:52:01 -0500

    180524

--- nodeAll/servidor.js
+++ nodeAll/servidor.js
@@ -1,38 +1,39 @@
 import express from "express";
 import { sumar } from "./src/suma.js";
+import bodyParser from "body-parser";
 
 //init
 const app = express();
 // middleware
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 //rutas
 app.get("/", (req, res) => {
   res.send("Hi, i am a server on express.. :) ");
-
 });
 app.get("/about", (req, res) => {
   res.send("About this project");
 });
 
-app.get("/calcular/:a/:b",  (req, res) => {
+app.get("/calcular/:a/:b", (req, res) => {
   const num1 = parseInt(req.params.a);
   const num2 = parseInt(req.params.b);
-    let resul= sumar(num1,num2);
-    console.log(resul)
-    res.send(resul.toString());
-  });
+  let resul = sumar(num1, num2);
+  console.log(resul);
+  res.send(resul.toString());
+});
 
-  app.post("/calcular", (req, res) => {
-    const num1 = parseInt(req.body.a);
-    const num2 = parseInt(req.body.b);
-    const resul = sumar(num1, num2);
-    res.send(resul.toString());
-  });
+app.post("/calcular", (req, res) => {
+  const num1 = parseInt(req.body.a);
+  const num2 = parseInt(req.body.b);
+  const resul = sumar(num1, num2);
+  res.send(resul.toString());
+});
 
-
-  
 app.use((req, res) => {
-    res.send("No found , sorry :(")
+  res.status(404).send("No found , sorry :(");
 });
 
 //servidor

81cd247d2c1b8245b04060e44b64b2f9351946ab
jegiraldp <jegiraldp@gmail.com>
2024-05-21 19:21:17 -0500

    210524

--- sitioweb/js/sumadora.js
+++ sitioweb/js/sumadora.js
@@ -1,4 +1,6 @@
-
-function calcular(){
-    alert("hola desde calcular");
+function calcular() {
+  let x = 5;
+  let y = 7;
+  let s = x + y;
+  document.getElementById("escondido").innerHTML = "La suma es " + s;
 }

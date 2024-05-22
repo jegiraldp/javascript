7e6058a339e5880327ca13340c3d1588bb09cee5
jegiraldp <jegiraldp@gmail.com>
2024-05-21 19:44:56 -0500

    210524

--- sitioweb/js/sumadora.js
+++ sitioweb/js/sumadora.js
@@ -1,6 +1,14 @@
 function calcular() {
-  let x = 5;
-  let y = 7;
-  let s = x + y;
-  document.getElementById("escondido").innerHTML = "La suma es " + s;
+  let n1 = document.getElementById("txtN1").value;
+  let n2 = document.getElementById("txtN2").value;
+
+  if (n1.length == 0 || n2.length == 0) {
+    alert("Faltan Datos");
+  } else {
+    let num1 = parseInt(n1);
+    let num2 = parseInt(n2);
+    let suma = num1 + num2;
+
+    document.getElementById("escondido").innerHTML = "La suma es " + suma;
+  }
 }
